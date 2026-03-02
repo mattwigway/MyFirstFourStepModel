06917304b3a135d526be10d0c112cd95fb2b038a
Matt Bhagat-Conway <matt@indicatrix.org>
2026-03-02 15:35:59 -0500

    organize gravity calibration

---
+++ R/gravity_calibration.R
@@ -0,0 +1,90 @@
+#' This calibrates the trip distance beta, using the method described in Merlin (2020)
+#' A new method using medians to calibrate single-parameter spatial interaction models, JTLU.
+#' The basic idea is that we adjust the beta for the decay function until half the distance-weighted
+#' accessibility occurs in the median travel distance.
+#' For NHB, productions and attractions will be the same
+calibrate_trip_distance_beta = function(productions, attractions, median_dist_km, dmat) {
+  # we assume that the betas for each trip type are the same at all time periods
+  origins = productions %>%
+    group_by(geoid) %>%
+    summarize(n_trips = sum(n_trips)) %>%
+    rename(orig_geoid = "geoid", orig_ntrips = "n_trips")
+
+  dests = attractions %>%
+    group_by(geoid) %>%
+    summarize(n_trips = sum(n_trips)) %>%
+    rename(dest_geoid = "geoid", dest_ntrips = "n_trips")
+
+  access = origins %>%
+    cross_join(dests) %>%
+    # avoid overflow
+    mutate(
+      weighted_attractions = as.double(dest_ntrips) *
+        (as.double(orig_ntrips) / as.double(mean(orig_ntrips)))
+    ) %>%
+    left_join(dmat, by = c("orig_geoid", "dest_geoid"))
+
+  stopifnot(all(access$weighted_attractions > 0))
+
+  opt_res = optim(
+    c(-2.0),
+    function(params) {
+      beta = params[[1]]
+
+      # calculate the objective function - the squared difference between
+      # the access before and the access after the median.
+      decayed_attractions = access$weighted_attractions * access$dist_km^beta
+      decayed_below = sum(decayed_attractions[access$dist_km < median_dist_km])
+      decayed_above = sum(decayed_attractions[access$dist_km >= median_dist_km])
+
+      # This is not quite the objective function in the Merlin paper. He did not divide by
+      # the sum of the decayed attractions. But doing so forces the function to have one global
+      # minimum. Otherwise, when beta gets really negative, the total attractiveness will be
+      # less than the difference was in other parts of the graph, and the algorithm may find
+      # the corner solution that minimizes total accessibility, rather than the balanced solution.
+      return(((decayed_below - decayed_above) / sum(decayed_attractions))^2)
+    },
+    method = "Brent",
+    lower = -20,
+    upper = 1
+  )
+
+  beta = opt_res$par[[1]]
+
+  if (opt_res$convergence != 0 || opt_res$value > 1e3 || beta <= -20 || beta >= 0) {
+    print(paste("WARN: calibration was unsatisfactory. Beta:", beta, "objective:", opt_res$value))
+  }
+
+  return(beta)
+}
+
+#' Calibrate betas for all trip types
+#' median_distances should be a list with element HBW, HBO, and NHB with median crow-flies
+#' trip distances in each.
+calibrate_trip_distance_betas = function(balanced, marginals, median_distances_m) {
+  dmat = get_distance_matrix(marginals)
+  return(list(
+    HBW = calibrate_trip_distance_beta(
+      filter(balanced$productions, trip_type == "HBW"),
+      filter(balanced$attractions, trip_type == "HBW"),
+      median_distances_m$HBW,
+      dmat
+    ),
+
+    HBO = calibrate_trip_distance_beta(
+      filter(balanced$productions, trip_type == "HBO"),
+      filter(balanced$attractions, trip_type == "HBO"),
+      median_distances_m$HBO,
+      dmat
+    ),
+
+    # NHB is a little different. The NHB productions are linked to the home location, so we
+    # use the attractions for both ends of the trip.
+    NHB = calibrate_trip_distance_beta(
+      filter(balanced$attractions, trip_type == "NHB"),
+      filter(balanced$attractions, trip_type == "NHB"),
+      median_distances_m$NHB,
+      dmat
+    )
+  ))
+}
